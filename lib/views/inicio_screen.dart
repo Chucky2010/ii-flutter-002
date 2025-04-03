dc0785df73e9f6877093f96ec8c29be4cc84cfab
Chucky2010 <sebas10ibarra81@gmail.com>
2025-04-03 15:37:52 -0300

    Se convierte la pantalla inicio_screen de la aplicación en un StatefulWidget
    Se añade un contador que aumenta cada vez que se presione el botón de incrementar

--- lib/views/inicio_screen.dart
+++ lib/views/inicio_screen.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 import 'acerca_de_screen.dart';
 import 'contacto_screen.dart';
 
-class InicioScreen extends StatelessWidget {
+class InicioScreen extends StatefulWidget {
   const InicioScreen({super.key});
 
   @override
+  _InicioScreenState createState() => _InicioScreenState();
+}
+
+class _InicioScreenState extends State<InicioScreen> {
+  int _counter = 0; // Variable de estado para el contador
+
+  @override
+  void initState() {
+    super.initState();
+    // Aquí puedes inicializar datos o realizar configuraciones necesarias
+    print('initState: El widget se ha creado');
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++; // Actualiza el estado del contador
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build: Dibujando la interfaz');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inicio'),
@@ -19,6 +40,16 @@ class InicioScreen extends StatelessWidget {
               '¡Bienvenido a la pantalla de Inicio!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Contador: $_counter',
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter, // Incrementa el contador
+              child: const Text('Incrementar contador'),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
